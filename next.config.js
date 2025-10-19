231fc4fc5f798dc2c4e6aa50626e9d4aa476e41f
Hiroaki Asakawa <hiroaki.asakawa@dsv.daikin.com>
2025-10-19 11:49:48 -0700

    downgrade to next 14 for use in bolt

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
+const nextConfig = {
     experimental: {
         // Enable this once it becomes available in latest version of next
         // Only available in canary as of now
